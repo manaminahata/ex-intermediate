cc3b5c0604cd24e9302f7f52ac6fd499a051d7e0
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-09 06:59:04 +0900

    コメントの追加

--- src/main/java/com/example/repository/BaseballRepository.java
+++ src/main/java/com/example/repository/BaseballRepository.java
@@ -36,15 +36,22 @@ public class BaseballRepository {
 		 */
 		public BaseballTeam load(Integer id) {
 			// SQL文の作成
+			// html上で表示させたい情報は球団名・本拠地・発足・歴史のみだが
+			// ResultSetを使っているときは全ての情報をSELECTしなければならない
 			String loadSql = "SELECT * "
 					+ " FROM teams WHERE id=:id";
 			
 			// プレースホルダに値を代入
+			// 今回は"id"に引数のidをもらう
 			SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 			
 			// SQLの実行
+			// 1つのオブジェクトのみの検索のため、「queryForObject」を使う
+			// baseballTeamオブジェクトに検索結果を代入する
 			BaseballTeam baseballTeam = template.queryForObject(loadSql, param, BASEBALL_TEAM_ROW_MAPPER);
 			
+			// baseballTeamオブジェクトにSQLの実行結果をリターンする
+			// baseballTeamオブジェクトはRowMapperで定義されているもの？
 			return baseballTeam;
 		}
 		
@@ -54,9 +61,12 @@ public class BaseballRepository {
 		 */
 		public List<BaseballTeam> findAll() {
 			// SQLの定義
+			// ORDER BY　idでidの昇順に表示されるようにする
 			String findAllSql = "SELECT * FROM teams ORDER BY id";
 			
 			// SQlの実行
+			// 全検索のため、2つ以上の情報となるので「query」
+			// 全件検索でプレースホルダに代入する必要がないため、SqlParameterSourceは必要ない
 			List<BaseballTeam> teamList = template.query(findAllSql, BASEBALL_TEAM_ROW_MAPPER);
 			
 			return teamList;
