f8610e4d5faea8745c026c4246806aaebbac9ec7
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-09 06:52:02 +0900

    コメントの追加

--- src/main/java/com/example/controller/BaseballController.java
+++ src/main/java/com/example/controller/BaseballController.java
@@ -14,36 +14,60 @@ import com.example.service.BaseballService;
 @Controller
 @RequestMapping("/teams")
 public class BaseballController {
+	// サービスクラスをオブジェクト化せず使えるようにDIで定義
+	// なぜ直接リポジトリと繋がない？
+	// 現在のリポジトリクラスはDBから値を受け取ったり追加や更新をするだけだが、
+	// 業務処理が追加されたときサービスを使うためサービスクラスを介してコントローラーが処理をしている？
 	@Autowired
 	private BaseballService baseballService;
 	
+	// なんのために定義している？
 	@ModelAttribute
 	public BaseballForm baseballSetUpForm() {
 		return new BaseballForm();
 	}
 	
 	/**
-	 * 野球チーム一覧を表示させる
-	 * @return
+	 * team-list.htmlで野球チーム一覧を表示させる
+	 * 
+	 * URLにidを直書きしてリンク先を識別することができるが
+	 * データが増えた際にいちいち追加することは不便なので
+	 * 繰り返し処理ができるようにListをリクエストスコープに格納する
+	 * 
+	 * @return team-list.html
 	 */
 	@RequestMapping("")
 	public String index(Model model) {
+		// baseballTeamListに全件検索結果（テーブル全ての情報）を代入
 		List<BaseballTeam> baseballTeamList = baseballService.showList();
+		
+		// リクエストスコープにbaseballTeamListを格納
 		model.addAttribute("baseballTeamList", baseballTeamList);
+		
+		// チーム一覧を表示させるHTMLをリターンする
 		return "team-list";
 	}
 	
+	/**
+	 * 受け取ったidでチームを識別し、詳細情報を表示するメソッド
+	 * @param id
+	 * @param model
+	 * @return name.htmlの表示
+	 */
 	@RequestMapping("/name")
 	public String teamName(Integer id, Model model) {
-		// ドメインのインスタンス化
+		// ドメインクラスのインスタンス化
 		BaseballTeam baseballTeam = new BaseballTeam();
 		
-		// ドメインクラスのgetIdを使って該当の野球チームを検索する
+		// 引数のidを使ってリンク先のデータを識別する
+		// リンクを開くことでshowDetailに引数のidが代入される
 		baseballTeam = baseballService.showDetail(id);
 		
-		// リクエストスコープに格納する
+		// そのまま値の受け渡しをすることができないため、
+		//　リクエストスコープに格納する
 		model.addAttribute("baseballTeam", baseballTeam);
 		
+		// チームの詳細情報を表示させるHTMLをリターンする
 		return "name";
 	}
 
