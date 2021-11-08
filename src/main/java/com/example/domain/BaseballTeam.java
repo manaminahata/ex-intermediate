e916112eac53a95bc30deb3bf52b5d1ae3399d95
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-08 15:15:11 +0900

    ドメインクラス（BaseballTeam.java）の作成

---
+++ src/main/java/com/example/domain/BaseballTeam.java
@@ -0,0 +1,61 @@
+package com.example.domain;
+
+
+public class BaseballTeam {
+	// id
+	private Integer id;
+	// リーグ名
+	private String leagueName;
+	// 球団名
+	private String teamName;
+	// 本拠地
+	private String headquarters;
+	// 発足
+	private String inaguration;
+	// 歴史
+	private String history;
+	
+	// getter/setterを定義
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLeagueName() {
+		return leagueName;
+	}
+	public void setLeagueName(String leagueName) {
+		this.leagueName = leagueName;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public String getHeadquarters() {
+		return headquarters;
+	}
+	public void setHeadquarters(String headquarters) {
+		this.headquarters = headquarters;
+	}
+	public String getInaguration() {
+		return inaguration;
+	}
+	public void setInaguration(String inaguration) {
+		this.inaguration = inaguration;
+	}
+	public String getHistory() {
+		return history;
+	}
+	public void setHistory(String history) {
+		this.history = history;
+	}
+	
+	@Override
+	public String toString() {
+		return "BaseballTeam [id=" + id + ", leagueName=" + leagueName + ", teamName=" + teamName + ", headquarters="
+				+ headquarters + ", inaguration=" + inaguration + ", history=" + history + "]";
+	}
+}
